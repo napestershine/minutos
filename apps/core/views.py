6fdf9fd1243fc7a7ab097c13435871f8024897d7
manu <napestershine@gmail.com>
2022-01-26 17:20:53 +0100

    home page done

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def frontpage(request):
+    return render(request, 'core/frontpage.html')
+
+
+def privacy(request):
+    return render(request, 'core/privacy.html')
+
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
--- minutos/urls.py
+++ minutos/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.core.views import frontpage, terms, contact, privacy
+
 urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('privacy', privacy, name='privacy'),
+    path('contact', contact, name='contact'),
+    path('terms', terms, name='terms'),
     path('admin/', admin.site.urls),
 ]
