b081a57aaeffb056f73d35000733aef0c6caa914
manu <napestershine@gmail.com>
2022-01-30 12:16:41 +0100

    created teams model and migrations

--- apps/team/admin.py
+++ apps/team/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.team.models import Team
+
+admin.site.register(Team)
--- minutos/settings.py
+++ minutos/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core',
-    'apps.userprofile'
+    'apps.userprofile',
     'apps.team'
 ]
 
